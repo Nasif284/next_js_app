096de11c7b6a8e897f6a40bcf6ef66a723d88f8c
Muhammad Nasif <nasifhima@gmail.com>
2026-03-18 13:29:24 +0530

    inalid api in users fixed

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Users = async () => {
-  const res = await fetch("http://localhost:3000/users/api/clients");
-  const { users } = await res.json()
-  return (
-    <>
-      <div>Users:</div>
-      {users.map((u: any) => (
-        <li key={u.id || u.name}>{u.name}</li>
-      ))}
-    </>
-  );
+  // const res = await fetch("http://localhost:3000/users/api/clients");
+  // const { users } = await res.json()
+  // return (
+  //   <>
+  //     <div>Users:</div>
+  //     {users.map((u: any) => (
+  //       <li key={u.id || u.name}>{u.name}</li>
+  //     ))}
+  //   </>
+  // );
 }
 
 export default Users
